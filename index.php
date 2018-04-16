341f13629c2780de656b4ec06f51c50f82cce270
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-04-16 12:27:01 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	get_template_part('inc/page-header');
 	echo '<div class="container">
         	<div class="row">';
-		get_template_part('loop-blog');
+		get_template_part('inc/loop-blog');
 		get_sidebar();
 	echo '	</div><!-- /.row -->
     	</div><!-- /.container -->';
