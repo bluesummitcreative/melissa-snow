0210d6f791aeac2fecd47996051ee68207d3c29f
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-04-16 12:22:02 -0600

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url( '/' ); ?>">
+<form role="search" method="get" id="searchform" class="input-group" action="<?php echo home_url( '/' ); ?>">
 	<input type="text" class="form-control framed" value="<?php echo get_search_query() ?>" name="s" placeholder="<?php echo esc_attr_x( 'Enter Keyword', 'placeholder') ?>">
 	<span class="input-group-btn"><button class="btn btn-primary" type="submit" value="<?php echo esc_attr_x( 'Search') ?>"><i class="fa fa-search"></i></button></span>
 </form>	
