5ccec854886e0cca720266aa22133b67e93449af
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-04-16 12:01:05 -0600

    Add files via upload

---
+++ searchform.php
@@ -0,0 +1,2 @@
+<input type="text" class="form-control framed" placeholder="Enter Keyword">
+				<span class="input-group-btn"><button class="btn btn-primary" type="button"><i class="fa fa-search"></i></button></span>
