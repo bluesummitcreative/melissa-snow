a3e518f84d19c5dfa1e1dba333e5d9fc57f34a56
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-09-07 16:45:55 -0600

    Update home-steps.php

--- inc/home-steps.php
+++ inc/home-steps.php
@@ -2,70 +2,23 @@
 			<div class="container">
 				<h2 class="center">Five Steps to Your Success</h2>
 				<div class="steps">
+					<?php if( have_rows('steps') ): $counter = 1; while ( have_rows('steps') ) : the_row(); ?>
 					<div class="step width-20">
 						<figure>
-							<aside>1.</aside>
+							<aside><?php echo $counter; ?>.</aside>
 							<div class="bar height-20">
 								<div class="arrow"></div>
 							</div>
 						</figure>
-						<h3>Contact Me</h3>
+						<h3><?php the_sub_field('step_title'); ?></h3>
 						<hr>
-						<p>Sed iaculis dapibus tellus eget condimentum. Curabitur ut tellus congue, convallis tortor et</p>
+						<p><?php the_sub_field('step_description'); ?></p>
 					</div>
-					<!--/ .step-->
-					<div class="step width-20">
-						<figure>
-							<aside>2.</aside>
-							<div class="bar height-40">
-								<div class="arrow"></div>
-							</div>
-						</figure>
-						<h3>Make an Appointment</h3>
-						<hr>
-						<p>Sed iaculis dapibus tellus eget condimentum. Curabitur ut tellus congue, convallis tortor et</p>
-					</div>
-					<!--/ .step-->
-					<div class="step width-20">
-						<figure>
-							<aside>3.</aside>
-							<div class="bar height-60">
-								<div class="arrow"></div>
-							</div>
-						</figure>
-						<h3>I will Analyze Your Profile</h3>
-						<hr>
-						<p>Sed iaculis dapibus tellus eget condimentum. Curabitur ut tellus congue, convallis tortor et</p>
-					</div>
-					<!--/ .step-->
-					<div class="step width-20">
-						<figure>
-							<aside>4.</aside>
-							<div class="bar height-80">
-								<div class="arrow"></div>
-							</div>
-						</figure>
-						<h3>Apply Changes</h3>
-						<hr>
-						<p>Sed iaculis dapibus tellus eget condimentum. Curabitur ut tellus congue, convallis tortor et</p>
-					</div>
-					<!--/ .step-->
-					<div class="step width-20">
-						<figure>
-							<aside>5.</aside>
-							<div class="bar height-100">
-								<div class="arrow"></div>
-							</div>
-						</figure>
-						<h3>Apply Changes</h3>
-						<hr>
-						<p>Sed iaculis dapibus tellus eget condimentum. Curabitur ut tellus congue, convallis tortor et</p>
-					</div>
-					<!--/ .step-->
+					<?php $counter++; endwhile; endif;?>
 				</div>
 				<!--/ .steps-->
 				<div class="center">
-					<a href="meet-me.html" class="btn btn-default btn-big">Contact Me!</a>
+					<a href="/contact/" class="btn btn-default btn-big">Contact Me!</a>
 				</div>
 			</div>
 			<!--/ .container-->
