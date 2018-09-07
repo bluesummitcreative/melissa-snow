5b0dbd8a28d79661b8345774fed549e875536682
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-09-07 15:36:57 -0600

    Update home-form.php

--- inc/home-form.php
+++ inc/home-form.php
@@ -3,7 +3,7 @@
 				<h2 class="center">Want to improve the relationships in your life?</h2>
 				<h3 class="center">Enter your name and email and I will send you my free e-book, <strong>“Ten Secrets to Having the Love You Want”</strong></h3>
 				<div class="row" style="background:#acbdc0;">
-					<div class="col-md-10 col-sm-10 col-md-offset-1 col-sm-offset-1">
+					<div class="col-md-10 col-sm-10 col-md-offset-3 col-sm-offset-3">
 						<?php echo do_shortcode('[gravityform id="1" title="false" description="false" ajax="true" tabindex="10"]'); ?>
 					</div>
 					<!--/ .col-md-10-->
