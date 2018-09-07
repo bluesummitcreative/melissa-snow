487d09d5843bf18dbeb8010d7e671be1b94c397e
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-09-07 13:58:09 -0600

    Update home-slider.php

--- inc/home-slider.php
+++ inc/home-slider.php
@@ -4,7 +4,7 @@
                <li>
                    <img src="/wp-content/uploads/Melissa-Snow-Header-2018-001.jpg" alt="">
                    <div class="tp-caption"
-                        data-x="left" data-hoffset="0"
+                        data-x="left" data-hoffset="202"
                         data-y="top" data-voffset="400"
                         data-transform_idle="o:1;"
                         data-transform_in="x:50px;opacity:0;s:700;e:Power3.easeInOut;"
@@ -12,7 +12,7 @@
                    </div>
                    <div class="tp-caption"
                         data-x="left" data-hoffset="0"
-                        data-y="top" data-voffset="520"
+                        data-y="top" data-voffset="521"
                         data-transform_idle="o:1;"
                         data-transform_in="x:50px;opacity:0;s:700;e:Power3.easeInOut;"
                         data-start="700"><a href="#five-steps-to-success" class="btn btn-primary">Apply for Coaching</a>
