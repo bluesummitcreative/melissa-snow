0b2daa943b4e9c2639044a149fef612e3639a8a1
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-09-07 16:21:46 -0600

    Update home-about.php

--- inc/home-about.php
+++ inc/home-about.php
@@ -4,52 +4,11 @@
             <div class="row">
                 <div class="col-md-6 col-sm-6">
                     <div class="center">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/author.jpg" class="circle" alt="">
+                        <img src="<?php the_field('about_image'); ?>" class="circle" alt="">
                     </div>
                 </div>
                 <div class="col-md-6 col-sm-6">
-                    <h3 class="no-bottom-margin"><strong>Jane Doe</strong></h3>
-                    <h5>A personal mentor</h5>
-                    <br>
-                    <p>
-                        Sed iaculis dapibus tellus eget condimentum. Curabitur ut tellus congue, convallis tortor et,
-                        pellentesque diam. Nullam non dolor eu ligula ultrices pellentesque placerat imperdiet metus.
-                        Etiam lobortis bibendum egestas. In quis massa a felis molestie consequat rhoncus vitae nisl.
-                        In pharetra posuere dictum. In eget metus eu leo rutrum venenatis vitae sit amet elit.
-                        Duis luctus enim enim.
-                    </p>
-                    <div class="numbers">
-                        <div class="row">
-                            <div class="col-md-4 col-sm-4">
-                                <div class="number">
-                                    <figure>68</figure>
-                                    <aside>Persons Mentored</aside>
-                                </div>
-                                <!--/ .number-->
-                            </div>
-                            <!--/ .col-md-4-->
-                            <div class="col-md-4 col-sm-4">
-                                <div class="number">
-                                    <figure>45</figure>
-                                    <aside>Workshops Attended</aside>
-                                </div>
-                                <!--/ .number-->
-                            </div>
-                            <!--/ .col-md-4-->
-                            <div class="col-md-4 col-sm-4">
-                                <div class="number">
-                                    <figure>12</figure>
-                                    <aside>Coaching Certificates</aside>
-                                </div>
-                                <!--/ .number-->
-                            </div>
-                            <!--/ .col-md-4-->
-                        </div>
-                        <!--/ .row-->
-                    </div>
-                    <!--/ .numbers-->
-                    <hr>
-                    <a href="" class="link arrow">More about me</a>
+                    <?php the_field('about_text'); ?>
                 </div>
                 <!--/ .col-sm-6-->
             </div>
