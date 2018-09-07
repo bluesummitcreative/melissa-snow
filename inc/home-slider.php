2a4f8da112150eba2be0adea3407559ec9068317
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-09-07 13:52:41 -0600

    Update home-slider.php

--- inc/home-slider.php
+++ inc/home-slider.php
@@ -5,14 +5,14 @@
                    <img src="/wp-content/uploads/Melissa-Snow-Header-2018-001.jpg" alt="">
                    <div class="tp-caption"
                         data-x="left" data-hoffset="0"
-                        data-y="top" data-voffset="500"
+                        data-y="top" data-voffset="400"
                         data-transform_idle="o:1;"
                         data-transform_in="x:50px;opacity:0;s:700;e:Power3.easeInOut;"
                         data-start="400"><h1>Melissa M Snow.<br /><span>Certified Relationship Coach</span></h1>
                    </div>
                    <div class="tp-caption"
                         data-x="left" data-hoffset="0"
-                        data-y="top" data-voffset="510"
+                        data-y="top" data-voffset="520"
                         data-transform_idle="o:1;"
                         data-transform_in="x:50px;opacity:0;s:700;e:Power3.easeInOut;"
                         data-start="700"><a href="#five-steps-to-success" class="btn btn-primary">Apply for Coaching</a>
