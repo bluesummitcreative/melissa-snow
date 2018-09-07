c740522efb009f0d08c654df7aee7281940b7bbb
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-09-07 15:31:55 -0600

    Update home-form.php

--- inc/home-form.php
+++ inc/home-form.php
@@ -1,35 +1,10 @@
 <div class="block">
 			<div class="container">
-				<h2 class="center">Do You Want a Successful Life?</h2>
-				<hr>
-				<h3 class="center">Enter your name and e-mail and I will tell you for free!</h3>
+				<h2 class="center">Want to improve the relationships in your life?</h2>
+				<h3 class="center">Enter your name and email and I will send you my free e-book, <strong>“Ten Secrets to Having the Love You Want”</strong></h3>
 				<div class="row">
 					<div class="col-md-10 col-sm-10 col-md-offset-1 col-sm-offset-1">
-						<form id="form-subscribe">
-							<div class="row">
-								<div class="col-md-5">
-									<div class="form-group">
-										<input type="text" class="form-control framed" id="name" name="name" placeholder="Your Name">
-									</div>
-								</div>
-								<!--/ .col-md-5-->
-								<div class="col-md-5">
-									<div class="form-group">
-										<input type="email" class="form-control framed" id="email" name="email" placeholder="Your E-mail">
-									</div>
-								</div>
-								<!--/ .col-md-5-->
-								<div class="col-md-2">
-									<div class="form-group">
-										<button type="submit" class="btn btn-primary width-100">Send Me</button>
-									</div>
-								</div>
-								<!--/ .col-md-2-->
-								<div class="form-status"></div>
-							</div>
-							<!--/ .row-->
-						</form>
-						<!--/ form-->
+						<?php echo do_shortcode('[gravityform id="1" title="false" description="false" ajax="true" tabindex="10"]'); ?>
 					</div>
 					<!--/ .col-md-10-->
 				</div>
