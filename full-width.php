ca6f0a5a2eb5d5cf1fb64f1f48bbebee2d0e2e48
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-10-11 18:58:31 -0600

    Create full-width.php

---
+++ full-width.php
@@ -0,0 +1,12 @@
+<?php
+  /*Full-Width, No Sidebar*/
+	get_header(); 
+	get_template_part('inc/page-header');
+	echo '<div class="container">
+        	<div class="row">';
+		get_template_part('inc/loop');
+		get_sidebar();
+	echo '	</div><!-- /.row -->
+    	</div><!-- /.container -->';
+	get_footer();
+?>
