7763f0af897b86cd9e1aa2a4923bdf5eb16ee9ac
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-10-11 18:36:29 -0600

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -1,16 +1,17 @@
 <?php 
 	get_header();
-	get_template_part('inc/home-slider');
-		echo '<div class="page-content">';
-			get_template_part('inc/home-features');
-			//get_template_part('inc/home-quote');
-			get_template_part('inc/home-about');
-			//get_template_part('inc/home-freebie');
-			get_template_part('inc/home-form');
-			get_template_part('inc/home-steps');
-			//get_template_part('inc/home-appointment');
-			//get_template_part('inc/home-testimonials');
-			//get_template_part('inc/home-packages');
-		echo '</div>';
+	echo do_shortcode('[rev_slider alias="home_slider"]');
+	//get_template_part('inc/home-slider');
+	echo '<div class="page-content">';
+		get_template_part('inc/home-features');
+		//get_template_part('inc/home-quote');
+		get_template_part('inc/home-about');
+		//get_template_part('inc/home-freebie');
+		get_template_part('inc/home-form');
+		get_template_part('inc/home-steps');
+		//get_template_part('inc/home-appointment');
+		//get_template_part('inc/home-testimonials');
+		//get_template_part('inc/home-packages');
+	echo '</div>';
     get_footer(); 
 ?>
