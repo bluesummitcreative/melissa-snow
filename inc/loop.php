dfdd7fbe2aecfcc3fb0ccb931b99948816ea3203
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-10-11 18:59:26 -0600

    Update loop.php

--- inc/loop.php
+++ inc/loop.php
@@ -1,5 +1,9 @@
 <!-- Content -->
-<div class="col-md-8 col-sm-8">
+	<?php if ( is_page_template('full-width.php') ) { ?>
+	<div class="col-sm-12">
+	<?php } else { ?>
+	<div class="col-md-8 col-sm-8">
+		<?php } ?>
 	<section id="content">
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<?php the_content(); ?>
