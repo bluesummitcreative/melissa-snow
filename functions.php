51f5700ee71c8f505af6c22943ecd1891380e81c
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-10-11 18:24:30 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 	require_once('functions/custom-walker.php');
 	require_once('functions/enqueue-styles-scripts.php');
+	require_once('functions/gravity-forms.php');
 	require_once('functions/register-menus.php');	
 	require_once('functions/register-sidebars.php');
 	require_once('functions/theme-setup.php');
