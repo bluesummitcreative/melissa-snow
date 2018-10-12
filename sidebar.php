bfaf65fceaad39f7d8c13f86f6ecd5e41298d665
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-10-11 18:17:38 -0600

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -2,7 +2,7 @@
 <div class="col-md-4 col-sm-4">
 	<section id="sidebar" class="sidebar">
 		<aside id="sidebar-cta">
-			<?php echo do shortcode('[gravityform id="3" title="false" description="false" ajax="true" tabindex="10"]'); ?>
+			<?php echo do_shortcode('[gravityform id="3" title="false" description="false" ajax="true" tabindex="10"]'); ?>
 		</aside>
 		<aside id="search">
 			<header><h3>Search</h3></header>
