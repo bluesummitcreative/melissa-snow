1d5202700feb6fb9fffe715a140c5a4303e6e9de
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-11-21 10:23:48 -0700

    Update home-about.php

--- inc/home-about.php
+++ inc/home-about.php
@@ -2,12 +2,7 @@
         <div class="container">
             <h2>About Me</h2>
             <div class="row">
-                <div class="col-md-6 col-sm-6">
-                    <div class="center">
-                        <img src="<?php the_field('about_image'); ?>" class="circle" alt="">
-                    </div>
-                </div>
-                <div class="col-md-6 col-sm-6">
+                <div class="col-sm-12">
                     <?php the_field('about_text'); ?>
                 </div>
                 <!--/ .col-sm-6-->
