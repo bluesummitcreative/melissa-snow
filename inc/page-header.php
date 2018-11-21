2ee6718d63fdf05f34ceab6223899aceaf88216b
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-11-21 09:47:08 -0700

    Update page-header.php

--- inc/page-header.php
+++ inc/page-header.php
@@ -1,5 +1,18 @@
 <div class="heading">
 	<div class="container">
-		<h1><?php the_title(); ?></h1>
+		<h1><?php
+			if ( is_home() ) { 
+				echo 'Blog';
+			/*} elseif ( is_404() ) { 
+				echo '404 Not Found';*/
+			} elseif ( is_post_type_archive() ) {
+				echo post_type_archive_title();
+			} elseif ( is_search() ) { 
+				echo $wp_query->found_posts . ' results found for "' . esc_html( get_search_query( false ) ) .'"';
+
+			} else {
+				the_title();
+			}
+		?></h1>
 	</div>
 </div>
