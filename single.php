8201b7a751622836ac68bcc436c423bad7667cd8
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-11-21 09:41:39 -0700

    Create single.php

---
+++ single.php
@@ -0,0 +1,11 @@
+<?php 
+	get_header(); 
+	get_template_part('inc/page-header');
+	echo '<div class="container">
+        	<div class="row">';
+		get_template_part('inc/loop');
+		get_sidebar();
+	echo '	</div><!-- /.row -->
+    	</div><!-- /.container -->';
+	get_footer();
+?>
