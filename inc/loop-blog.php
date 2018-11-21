f033ecf4f54b26c50a41bf944e2a4341b2936fb2
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-11-21 10:38:16 -0700

    Update loop-blog.php

--- inc/loop-blog.php
+++ inc/loop-blog.php
@@ -23,13 +23,6 @@
 		<!-- Pagination -->
 		<div class="center">
 			<?php wpex_pagination(); ?>
-			<ul class="pagination">
-				<li class="active"><a href="#">1</a></li>
-				<li><a href="#">2</a></li>
-				<li><a href="#">3</a></li>
-				<li><a href="#">4</a></li>
-				<li><a href="#">5</a></li>
-			</ul><!-- /.pagination-->
 		</div><!-- /.center-->
 	</section><!-- /#content -->
 </div><!-- /.col-md-9 -->
