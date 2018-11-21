53427ade6f11111524b81e8eff97bbeea0fc05b2
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-11-21 09:31:50 -0700

    Update loop-blog.php

--- inc/loop-blog.php
+++ inc/loop-blog.php
@@ -4,18 +4,18 @@
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<article class="blog-post">
 			<?php if(has_post_thumbnail()) { ?>
-				<a href="<?php the_permalink() ?>"><?php the_post_thumbnail(array('class' => 'img-responsive'));?></a>
+				<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array('class' => 'img-responsive'));?></a>
 			<?php } else {}?>
-			<header><a href="<?php the_permalink() ?>"><h2><?php the_title(); ?></h2></a></header>
+			<header><h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2></header>
 			<figure class="meta">
-				<a href="#" class="link-icon"><i class="fa fa-user"></i><?php the_author(); ?></a>
-				<a href="#" class="link-icon"><i class="fa fa-calendar"></i><?php the_time( get_option( 'date_format' ) ); ?></a>
+				<i class="fa fa-user"></i><?php the_author(); ?>
+				<i class="fa fa-calendar"></i><?php the_time( get_option( 'date_format' ) ); ?>
 				<div class="tags">
 					<?php the_category(', '); ?>
 				</div>
 			</figure>
 			<?php the_excerpt(); ?>
-			<a href="<?php the_permalink() ?>" class="btn btn-default btn-small btn-grey">Read More</a>
+			<a href="<?php the_permalink(); ?>" class="btn btn-default btn-small btn-grey">Read More</a>
 		</article><!-- /.blog-post -->
 		<?php endwhile; endif; ?>
 		
