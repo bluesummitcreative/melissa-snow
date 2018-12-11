c8fbd046794c60b1529b38774c1d3e8591de025b
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-12-11 14:33:08 -0700

    Update loop.php

--- inc/loop.php
+++ inc/loop.php
@@ -5,7 +5,11 @@
 	<div class="col-md-8 col-sm-8">
 		<?php } ?>
 	<section id="content">
+		
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<?php if(has_post_thumbnail()) { ?>
+				<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('full',array('class' => 'img-responsive'));?></a>
+			<?php } else {}?>
 			<?php the_content(); ?>
 		<?php endwhile; endif; ?>
 	</section><!-- /#content -->
