262d7085fbfc2db7aff91614c570bd5dda8c6622
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-12-11 14:35:36 -0700

    Update loop-blog.php

--- inc/loop-blog.php
+++ inc/loop-blog.php
@@ -4,20 +4,25 @@
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<article class="blog-post">
 			<div class="row">
+			<?php if(has_post_thumbnail()) { ?>
+				<div class="col-md-6">
+				<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium',array('class' => 'img-responsive'));?></a>
+				</div>
+				<div class="col-md-6">
+			<?php } else { ?>
 				<div class="col-md-12">
-					<?php if(has_post_thumbnail()) { ?>
-						<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium',array('class' => 'img-responsive'));?></a>
-					<?php } else {}?>
-					<header><h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2></header>
-					<figure class="meta">
-						<i class="fa fa-user"></i> <?php the_author(); ?>&nbsp;
-						<i class="fa fa-calendar"></i> <?php the_time( get_option( 'date_format' ) ); ?>
-						<div class="tags">
-							<?php the_category(', '); ?>
-						</div>
-					</figure>
-					<?php the_excerpt(); ?>
-					<a href="<?php the_permalink(); ?>" class="btn btn-default btn-small btn-grey">Read More</a>
+			<?php }?>
+
+			<header><h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2></header>
+			<figure class="meta">
+				<i class="fa fa-user"></i> <?php the_author(); ?>&nbsp;
+				<i class="fa fa-calendar"></i> <?php the_time( get_option( 'date_format' ) ); ?>
+				<div class="tags">
+					<?php the_category(', '); ?>
+				</div>
+			</figure>
+			<?php the_excerpt(); ?>
+			<a href="<?php the_permalink(); ?>" class="btn btn-default btn-small btn-grey">Read More</a>
 				</div>
 			</div>
 		</article><!-- /.blog-post -->
